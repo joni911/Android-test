ba4594e2e31aed01a9b452c0ce3cfa0fc24806e9
Ubuntu <ubuntu@localhost.localdomain>
2026-06-17 11:18:05 +0800

    Fix: Add plugin versions in root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,4 @@
-// Root build file - repositories sudah didefinisi di settings.gradle.kts
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
